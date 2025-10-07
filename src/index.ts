7b5bea9d3f61f3307452257bda2be49df1051c82
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-10-07 14:03:09 +0600

    still fixing issue for render deployment in package.json

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/library";
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 mongoose
   .connect(MONGO_URI)
   .then(() => {
